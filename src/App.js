125e0938116985277a94afe7b1637fe2c6d4ccb2
Macmaxwell <82811326+Macmaxwell@users.noreply.github.com>
2021-09-26 17:29:24 +0200

    Pokedex Completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from "./logo.svg";
-import "./App.css";
 import Logo from "./Logo";
 import BestPokemon from "./BestPokemon";
 import CaughtPokemon from "./CaughtPokemon";
+import PokemonMoveSelector from "./PokemonMoveSelector";
+import PokemonCity from "./PokemonCity";
 
 const App = () => {
-
   const logWhenClicked = () => {
     console.log("Hakuna matata");
   };
   return (
     <div>
-      <Logo handleClick={logWhenClicked} appName="Pokedex" />
+      <Logo appName="Pokedex" handleClick={logWhenClicked} />
       <BestPokemon
         abilities={["Anticipation", "Adaptability", "Run-Away"]}
         name="Squirtle"
         color="yellow"
       />
       <CaughtPokemon date={new Date().toLocaleDateString()} />
+      <PokemonMoveSelector />
+      <PokemonCity />
     </div>
   );
 };
--- src/CaughtPokemon.js
+++ src/CaughtPokemon.js
@@ -1,50 +1,57 @@
- import { useState } from 'react'
+import { useState } from "react";
 
 const pokemonNames = [
-  "Dragonair", 
+  "Dragonair",
   "Mew",
-  "Dragonite", 
-  "Steelix", 
+  "Dragonite",
+  "Steelix",
   "Gengar",
   "Solrock",
-  "Pichu" ,
-  "Regirock", 
-  "Regice", 
+  "Pichu",
+  "Regirock",
+  "Regice",
   "Registeel",
   "Rhydon",
-  
-]
-
-
+];
 const CaughtPokemon = (props) => {
-  const [ caught, setCaught ] = useState([])
+  const [caught, setCaught] = useState([]);
+  const [pokemonNameInput, setPokemontNameInput] = useState("");
 
   // PSEUDOCODE SOLUTION
   // catchPokemon adds a random pokemon to the state
   const catchPokemon = () => {
     // pick a random number between 0 and the number of pokemon names we have
-    let randomPosition = Math.floor(Math.random() * pokemonNames.length)
+    let randomPosition = Math.floor(Math.random() * pokemonNames.length);
+    //let randomPosition = Math.floor(Math.random() * pokemonNames.length)
     // get the pokemon name at that position in the pokemonNames array
-    const randomPokemon = pokemonNames[randomPosition]
+    const randomPokemon = pokemonNames[randomPosition];
+    //const randomPokemon = pokemonNames[randomPosition]
     // add that pokemon name to the caught array in the state
-    const newCaught = caught.concat(randomPokemon)
-    setCaught(newCaught)
-  }
+    const newCaught = caught.concat(pokemonNameInput);
 
+    setPokemontNameInput("");
+
+    setCaught(newCaught);
+  };
+
+  function handleInputChange(event) {
+    setPokemontNameInput(event.target.value);
+  }
   return (
-    <p>
-      <span>Caught {caught.length} Pokémon on {props.date}</span>
+    <span>
+      Caught {caught.length} Pokémon on {props.date}
       <ul>
-        { caught.map(name => (
-          <li>{name}</li>
+        {caught.map((name, index) => (
+          <li key={index}>{name}</li>
         ))}
       </ul>
+      <input
+        type="text"
+        value={pokemonNameInput}
+        onChange={handleInputChange}
+      />
       <button onClick={catchPokemon}>Catch a Pokemon</button>
-    </p>
-  )
-}
-
-
-
-export default CaughtPokemon
-
+    </span>
+  );
+};
+export default CaughtPokemon;
